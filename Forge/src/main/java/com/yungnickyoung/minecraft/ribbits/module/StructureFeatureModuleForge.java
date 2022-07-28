26556f1bc190a2d7c1864742ef9060f309456bae
Nick Young <yungnickyoung@gmail.com>
2022-07-27 22:38:36 -0400

    Fix broken structure feature loading for Forge.

--- Forge/src/main/java/com/yungnickyoung/minecraft/ribbits/module/StructureFeatureModuleForge.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/ribbits/module/StructureFeatureModuleForge.java
@@ -1,11 +1,11 @@
 package com.yungnickyoung.minecraft.ribbits.module;
 
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +14,16 @@ public class StructureFeatureModuleForge {
     public static Map<ResourceLocation, StructureFeature<? extends FeatureConfiguration>> STRUCTURE_FEATURES = new HashMap<>();
 
     public static void init() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(StructureFeatureModuleForge::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(StructureFeature.class, StructureFeatureModuleForge::registerStructures);
     }
 
-    private static void commonSetup(FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> STRUCTURE_FEATURES.forEach((name, structurePieceType) -> Registry.register(Registry.STRUCTURE_FEATURE, name, structurePieceType)));
+    private static void registerStructures(RegistryEvent.Register<StructureFeature<?>> event) {
+        IForgeRegistry<StructureFeature<?>> registry = event.getRegistry();
+        STRUCTURE_FEATURES.forEach((name, structureFeature) -> register(registry, name, structureFeature));
+    }
+
+    private static void register(IForgeRegistry<StructureFeature<?>> registry, ResourceLocation name, StructureFeature<? extends FeatureConfiguration> structureFeature) {
+        structureFeature.setRegistryName(name);
+        registry.register(structureFeature);
     }
 }
