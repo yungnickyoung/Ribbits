cff91985f18a771d7ff9a569c4f14b98f591c7c0
Nick Young <yungnickyoung@gmail.com>
2022-10-28 20:03:37 -0400

    Initial village generation!

--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/world/structure/RibbitVillage.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/ribbits/world/structure/RibbitVillage.java
@@ -1,8 +1,12 @@
 package com.yungnickyoung.minecraft.ribbits.world.structure;
 
 import com.yungnickyoung.minecraft.yungsapi.api.YungJigsawConfig;
+import com.yungnickyoung.minecraft.yungsapi.api.YungJigsawManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
 
 import java.util.Optional;
 
@@ -14,8 +18,14 @@ public class RibbitVillage extends StructureFeature<YungJigsawConfig> {
 
     public RibbitVillage() {
         super(YungJigsawConfig.CODEC, context -> {
-            // TODO
-            return Optional.empty();
+            BlockPos startPos = new BlockPos(context.chunkPos().getMiddleBlockX(), 0, context.chunkPos().getMiddleBlockZ());
+            return YungJigsawManager.assembleJigsawStructure(
+                    context,
+                    PoolElementStructurePiece::new,
+                    startPos,
+                    false,
+                    true,
+                    80);
         });
     }
 }
