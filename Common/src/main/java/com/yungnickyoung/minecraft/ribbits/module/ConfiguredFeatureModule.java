6348d0398ac46144c94964c3e0ffd4480d3d98b7
Nick Young <yungnickyoung@gmail.com>
2023-02-16 22:49:49 -0500

    Update YAPI to simplify AutoRegister wrappers for Configured/PlacedFeatures.

--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/ConfiguredFeatureModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/ConfiguredFeatureModule.java
@@ -8,7 +8,6 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
@@ -18,20 +17,19 @@ import java.util.List;
 @AutoRegister(RibbitsCommon.MOD_ID)
 public class ConfiguredFeatureModule {
     @AutoRegister("veg_patch")
-    public static final AutoRegisterConfiguredFeature VEG_PATCH = AutoRegisterConfiguredFeature.of(() ->
-            new ConfiguredFeature<>(
-                    Feature.FLOWER,
-                    new RandomPatchConfiguration(
-                            16, // tries
-                            6, // xz spread
-                            2, // y spread
-                            PlacementUtils.onlyWhenEmpty(FeatureModule.SIMPLE_PLANT_BLOCK, // PlacedFeature
-                                    new RibbitsSimplePlantBlockConfig(
-                                            new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
-                                                    .add(BlockModule.UMBRELLA_LEAF.get().defaultBlockState(), 1)
-                                                    .add(BlockModule.TOADSTOOL.get().defaultBlockState(), 3)
-                                                    .add(BlockModule.SWAMP_DAISY.get().defaultBlockState(), 1)
-                                                    .build()),
-                                            List.of(Blocks.PODZOL.defaultBlockState(),
-                                                    Blocks.COARSE_DIRT.defaultBlockState()))))));
+    public static final AutoRegisterConfiguredFeature<RandomPatchConfiguration> VEG_PATCH = AutoRegisterConfiguredFeature.of(
+            Feature.FLOWER,
+            () -> new RandomPatchConfiguration(
+                    16, // tries
+                    6, // xz spread
+                    2, // y spread
+                    PlacementUtils.onlyWhenEmpty(FeatureModule.SIMPLE_PLANT_BLOCK, // PlacedFeature
+                            new RibbitsSimplePlantBlockConfig(
+                                    new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
+                                            .add(BlockModule.UMBRELLA_LEAF.get().defaultBlockState(), 1)
+                                            .add(BlockModule.TOADSTOOL.get().defaultBlockState(), 3)
+                                            .add(BlockModule.SWAMP_DAISY.get().defaultBlockState(), 1)
+                                            .build()),
+                                    List.of(Blocks.PODZOL.defaultBlockState(),
+                                            Blocks.COARSE_DIRT.defaultBlockState())))));
 }
--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/PlacedFeatureModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/PlacedFeatureModule.java
@@ -3,16 +3,13 @@ package com.yungnickyoung.minecraft.ribbits.module;
 import com.yungnickyoung.minecraft.ribbits.RibbitsCommon;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegister;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegisterPlacedFeature;
-import net.minecraft.core.Holder;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 import java.util.List;
 
 @AutoRegister(RibbitsCommon.MOD_ID)
 public class PlacedFeatureModule {
     @AutoRegister("veg_patch")
-    public static final AutoRegisterPlacedFeature VEG_PATCH = AutoRegisterPlacedFeature.of(() ->
-            new PlacedFeature(
-                    Holder.direct(ConfiguredFeatureModule.VEG_PATCH.get()),
-                    List.of()));
+    public static final AutoRegisterPlacedFeature VEG_PATCH = AutoRegisterPlacedFeature.of(
+            ConfiguredFeatureModule.VEG_PATCH,
+            List.of());
 }
