735009988c9b73c59c4ad68e9ee72769c64e2c56
Nick Young <yungnickyoung@gmail.com>
2023-06-30 17:47:47 -0400

    Make Ribbits villages avoid overlapping swamp huts and vanilla villages.

--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/StructureFeatureModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/StructureFeatureModule.java
@@ -1,7 +1,7 @@
 package com.yungnickyoung.minecraft.ribbits.module;
 
 import com.yungnickyoung.minecraft.ribbits.RibbitsCommon;
-import com.yungnickyoung.minecraft.ribbits.world.structure.RibbitVillage;
+import com.yungnickyoung.minecraft.ribbits.world.structure.RibbitVillageStructure;
 import com.yungnickyoung.minecraft.yungsapi.api.YungJigsawConfig;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegister;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
@@ -9,5 +9,5 @@ import net.minecraft.world.level.levelgen.feature.StructureFeature;
 @AutoRegister(RibbitsCommon.MOD_ID)
 public class StructureFeatureModule {
     @AutoRegister("ribbit_village")
-    public static StructureFeature<YungJigsawConfig> RIBBIT_VILLAGE = new RibbitVillage();
+    public static StructureFeature<YungJigsawConfig> RIBBIT_VILLAGE = new RibbitVillageStructure();
 }
