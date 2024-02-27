45e4b300d909b64ddcad0bbdad297c321d0cf2b8
HellionGames <dsgillespie02@yahoo.com>
2024-02-27 16:10:14 -0500

    New assets, add merchant profession

--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/RibbitProfessionModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/ribbits/module/RibbitProfessionModule.java
@@ -21,6 +21,7 @@ public class RibbitProfessionModule {
     public static final RibbitProfession GARDENER = register("gardener", "geo/gardener_ribbit.geo.json");
     public static final RibbitProfession SORCERER = register("sorcerer", "geo/sorcerer_ribbit.geo.json");
     public static final RibbitProfession FISHERMAN = register("fisherman", "geo/fisherman_ribbit.geo.json");
+    public static final RibbitProfession MERCHANT = register("merchant", "geo/merchant_ribbit.geo.json");
 
     /**
      * Registers a RibbitProfession with the given name and model path.
