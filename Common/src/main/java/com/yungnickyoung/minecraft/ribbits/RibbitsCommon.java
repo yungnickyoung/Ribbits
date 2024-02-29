9354fa70f17e7ef1bbf0e168eefacd327cb1b90e
Nick Young <yungnickyoung@gmail.com>
2024-02-29 00:02:09 -0500

    Fix creative tabs.

--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/RibbitsCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/ribbits/RibbitsCommon.java
@@ -6,6 +6,7 @@ import com.yungnickyoung.minecraft.ribbits.services.Services;
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegister;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegisterCreativeTab;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.apache.logging.log4j.LogManager;
@@ -18,10 +19,27 @@ public class RibbitsCommon {
 
     public static final ConfigModule CONFIG = new ConfigModule();
 
-//    @AutoRegister("general")
-//    public static AutoRegisterCreativeTab TAB_RIBBITS = new AutoRegisterCreativeTab.Builder()
-//            .iconItem(() -> new ItemStack(BlockModule.RED_TOADSTOOL.get()))
-//            .build();
+    @AutoRegister("general")
+    public static AutoRegisterCreativeTab TAB = AutoRegisterCreativeTab.builder()
+            .title(Component.translatable("itemGroup.ribbits.general"))
+            .iconItem(() -> new ItemStack(BlockModule.RED_TOADSTOOL.get()))
+            .entries((params, output) -> {
+                output.accept(BlockModule.RED_TOADSTOOL.get());
+                output.accept(BlockModule.BROWN_TOADSTOOL.get());
+                output.accept(BlockModule.TOADSTOOL_STEM.get());
+                output.accept(BlockModule.SWAMP_LANTERN.get());
+                output.accept(BlockModule.GIANT_LILYPAD.get());
+                output.accept(BlockModule.SWAMP_DAISY.get());
+                output.accept(BlockModule.TOADSTOOL.get());
+                output.accept(BlockModule.UMBRELLA_LEAF.get());
+                output.accept(BlockModule.MOSSY_OAK_PLANKS.get());
+                output.accept(BlockModule.MOSSY_OAK_PLANKS.getStairs());
+                output.accept(BlockModule.MOSSY_OAK_PLANKS.getSlab());
+                output.accept(BlockModule.MOSSY_OAK_PLANKS.getFence());
+                output.accept(BlockModule.MOSSY_OAK_PLANKS.getFenceGate());
+                output.accept(BlockModule.MOSSY_OAK_DOOR.get());
+            })
+            .build();
 
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.ribbits");
